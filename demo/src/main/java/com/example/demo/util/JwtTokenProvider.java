4869f07408f76cc76c7b9a36e8839b87c4b17653
soyeon <ksy1118s@naver.com>
2024-12-08 14:17:08 +0900

    feat: 관리자 로그인 기능 구현

--- demo/src/main/java/com/example/demo/util/JwtTokenProvider.java
+++ demo/src/main/java/com/example/demo/util/JwtTokenProvider.java
@@ -2,6 +2,7 @@ package com.example.demo.util;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.spec.SecretKeySpec;
@@ -16,25 +17,13 @@ import java.util.Date;
 @Component
 public class JwtTokenProvider {
     // 비밀키
-    private static final String SECRET_KEY = "your-secret-key";
+    private final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
     // 토큰 만료 시간
     private static final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 hour
 
     /**
-     * 서명(Signature)에 사용할 Key 객체 생성
-     * SecretKeySpec을 사용하여 비밀키를 HMAC 알고리즘과 연결된 Key로 변환
-     *
-     * @return 서명에 사용할 Key 객체
-     */    private Key getSigningKey() {
-        return new SecretKeySpec(
-                SECRET_KEY.getBytes(StandardCharsets.UTF_8), // 비밀키를 바이트 배열로 변환
-                SignatureAlgorithm.HS256.getJcaName()        // 알고리즘 지정 (HMAC-SHA256)
-        );
-    }
-
-    /**
-     * 사용자 이메일을 기반으로 JWT 토큰 생성
+     * 사용자 이메일을 기반으로 JWT 토큰 생성.
      *
      * @param email 토큰의 주제(사용자 식별자)
      * @return 생성된 JWT 토큰 (String 형식)
@@ -44,7 +33,7 @@ public class JwtTokenProvider {
                 .setSubject(email)                                                     // 토큰의 주제
                 .setIssuedAt(new Date())                                               // 토큰 생성 시간
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME)) // 만료 시간
-                .signWith(getSigningKey(), SignatureAlgorithm.HS256)                   // 서명
+                .signWith(SECRET_KEY)                                                  // 서명
                 .compact();                                                            // JWT를 문자열 형태로 직렬화
     }
 }
